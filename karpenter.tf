5a1b225d9baec8f673d1bc09faac5eba279391e9
Krishan Shamod <krishanshamod@gmail.com>
2023-01-18 13:40:58 +0530

    Updated the karpenter config

--- karpenter.tf
+++ karpenter.tf
@@ -1,8 +1,3 @@
-# IAM service linked role for spot instances
-resource "aws_iam_service_linked_role" "spotInstanceRole" {
-  aws_service_name = "spot.amazonaws.com"
-}
-
 # Creates Karpenter native node termination handler resources and IAM instance profile
 module "karpenter" {
   source  = "terraform-aws-modules/eks/aws//modules/karpenter"
